5edf81f2802cdc82330e6432afe34addb9f5ab91
Twenty2Eleven78 <mark@e-missive.co.uk>
2025-02-13 23:56:03 +0000

    Update service worker

--- js/sw.js
+++ js/sw.js
@@ -2,15 +2,15 @@
 const CACHE_NAME = "nugt-cache-v1";
 //Files to cache
 const cacheFiles = [
-  '/',
-  '/index.html',
-  '/css/all.min.css',
-  '/css/bootstrap.min.css',
-  '/css/style.css',
-  '/js/bootstrap.bundle.min.js',
-  '/js/roster.js',
-  '/js/script.js',
-  '/nugt512.png',
+  './',
+  './index.html',
+  './css/all.min.css',
+  './css/bootstrap.min.css',
+  './css/style.css',
+  './js/bootstrap.bundle.min.js',
+  './js/roster.js',
+  './js/script.js',
+  './nugt512.png',
 ];
 
 self.addEventListener('install', event => {

9f3f30c1ce7f3a17b2f581a9ab6b62fc32fcd191
Twenty2Eleven78 <mark@e-missive.co.uk>
2025-02-14 00:21:23 +0000

    Update service worker

--- sw.js
+++ sw.js
@@ -13,16 +13,42 @@ const cacheFiles = [
   '/nugt512.png',
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(cacheFiles))
+self.addEventListener('install', function(event) {
+  // Perform install steps
+      event.waitUntil(
+          caches.open(CACHE_NAME)
+          .then(function(cache) {
+              console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+          })
+      );
+  });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['pigment'];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
